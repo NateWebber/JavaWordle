d6494b651fceb0a67f2caeaccdabcfd442b8ceb0
Nate Webber <nwebber@u.rochester.edu>
2022-01-09 11:51:29 -0500

    yellow filtering improves a lot

--- Wordle.java
+++ Wordle.java
@@ -1,15 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Wordle {
 
     static ArrayList<String> dictionary;
 
-    static ArrayList<Character>[] graySets;
+    static HashSet<Character>[] graySets;
 
-    static ArrayList<Character>[] yellowSets;
+    static HashSet<Character>[] yellowSets;
 
     static char[] solution;
 
@@ -29,10 +30,14 @@ public class Wordle {
             if ((currentResponse.equals("ggggg")))
                 break;
             processResponse(currentResponse, currentGuess);
+            // printYellowSets();
+            filterYellows();
+            // currentGuess = findMostGreens();
             currentGuess = dictionary.get(0);
         }
         System.out.println(
-                "I won! The solution was: \"" + currentGuess + "\". It took me " + guessCount + " guess(es). Good game!");
+                "I won! The solution was: \"" + currentGuess + "\". It took me " + guessCount
+                        + " guess(es). Good game!");
         reader.close();
     }
 
@@ -43,10 +48,12 @@ public class Wordle {
         while (fileReader.hasNextLine())
             dictionary.add(fileReader.nextLine());
         fileReader.close();
-        graySets = new ArrayList[5];
+        graySets = new HashSet[5];
         for (int i = 0; i < 5; i++)
-            graySets[i] = new ArrayList<Character>();
-        yellowSets = graySets.clone(); // they're empty right now, so this is harmless
+            graySets[i] = new HashSet<Character>();
+        yellowSets = new HashSet[5];
+        for (int i = 0; i < 5; i++)
+            yellowSets[i] = new HashSet<Character>();
         solution = new char[5];
     }
 
@@ -68,7 +75,12 @@ public class Wordle {
                     for (String s : dictionary)
                         if ((s.charAt(i) == guessChar))
                             removeList.add(s);
-                    yellowSets[i].add(guessChar);
+                    for (int j = 0; j < 5; j++) {
+                        if (j == i)
+                            continue;
+                        // System.out.printf("Adding %c to yellowSets[%d]\n", guessChar, j);
+                        yellowSets[j].add(guessChar);
+                    }
                     break;
                 case 'x':
                     for (String s : dictionary)
@@ -82,4 +94,55 @@ public class Wordle {
 
         }
     }
+
+    static void filterYellows() {
+        ArrayList<String> removeList = new ArrayList<>();
+        for (HashSet<Character> set : yellowSets)
+            for (Character c : set)
+                for (String s : dictionary)
+                    if (s.indexOf(c) == -1) {
+                        removeList.add(s);
+                        // System.out.printf("Going to remove %s because it didn't have %c\n", s, c);
+                    }
+        for (String s : removeList)
+            dictionary.remove(s);
+        // System.out.println("exited filterYellows");
+    }
+
+    static void printYellowSets() {
+        for (int i = 0; i < 5; i++) {
+            HashSet<Character> currSet = yellowSets[i];
+            System.out.printf("YELLOW SET %d :\n", i);
+            for (Character c : currSet)
+                System.out.println(c);
+        }
+    }
+
+    // static int countGreens(String word) {
+    // int retInt = 0;
+    // for (int i = 0; i < 5; i++) {
+    // if (solution[i] != '\u0000')
+    // if (word.charAt(i) == solution[i])
+    // retInt += 1;
+    // }
+    // return retInt;
+    // }
+
+    // static String findMostGreens() {
+    // int max = 0;
+    // String word = "";
+    // for (String s : dictionary) {
+    // int greens = countGreens(s);
+    // if (greens > max) {
+    // max = greens;
+    // word = s;
+    // }
+    // }
+    // if (word.equals("")) {
+    // System.out.println("No words in the dictionary had any greens!");
+    // return dictionary.get(0);
+    // }
+    // System.out.printf("%s had the most greens (%d)\n", word, max);
+    // return word;
+    // }
 }
